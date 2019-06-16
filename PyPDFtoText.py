f7d1d792ba3dfe3821fb55f6e0d4d0a5ec81aea7
Kaushal <50149591+Kaushal1011@users.noreply.github.com>
2019-06-17 04:33:34 +0530

    Create PyPDFtoText.py

---
+++ PyPDFtoText.py
@@ -0,0 +1,33 @@
+import os
+import fitz
+import pytesseract
+from PIL import Image as img
+
+
+def main():
+    """ main function """
+    print("Enter the name of Pdf File You want to convert to text: example "+"newwork.pdf")
+    filename = str(input())  # name of pdf file you want to render
+    N = 0
+    # render with PyMuPDF
+    doc = fitz.open(filename)
+    N = doc.pageCount
+    print("Enter the name of text file where you want to save")
+    filetext = open(str(input())+".txt", "a")
+    print("You May Enjoy a Coffee while the application converts all of pdf into text")
+    for i in range(N):
+        page = doc.loadPage(i)
+        pix = page.getPixmap()
+        pix.writePNG("out.png")
+        # creates a temporary image to process text
+        text = pytesseract.image_to_string(img.open('out.png'))
+        filetext.write(text)
+        print("completed {} out of {}".format(i + 1, N))
+    os.remove("out.png")
+    print("Task Completed")
+
+    filetext.close()
+
+
+if __name__ == "__main__":
+    main()
